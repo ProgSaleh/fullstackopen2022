c2b31b35d6e91fce1002b0b6af7f4c3615e498c7
ProgSaleh <progsaleh@hotmail.com>
2022-07-25 05:54:13 +0300

    solve: 1.12*: anecdotes step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,101 +1,34 @@
 import { useState } from "react";
 
-const FeedbackHead = ({ headText }) => <h1>{headText}</h1>;
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const StatText = ({ text }) => <h2>{text}</h2>;
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
 
-const StatisticsTable = (props) => {
-  const { valuesArr, textsArr, statTextArr, all, avg, positive } = props;
+  const [selected, setSelected] = useState(0);
+
+  const generateNum = () =>
+    Number(String(Math.random() * anecdotes.length).substring(0, 1));
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>{textsArr[0]}</td>
-          <td>{valuesArr[0]}</td>
-        </tr>
-        <tr>
-          <td>{textsArr[1]}</td>
-          <td>{valuesArr[1]}</td>
-        </tr>
-        <tr>
-          <td>{textsArr[2]}</td>
-          <td>{valuesArr[2]}</td>
-        </tr>
-        <tr>
-          <td>{statTextArr[0]}</td>
-          <td>{all}</td>
-        </tr>
-        <tr>
-          <td>{statTextArr[1]}</td>
-          <td>{avg}</td>
-        </tr>
-        <tr>
-          <td>{statTextArr[2]}</td>
-          <td>{positive}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button
+        handleClick={() => setSelected(generateNum)}
+        text="next anecdote"
+      />
+    </div>
   );
-};
-
-const NoFeedback = ({ text }) => <p>{text}</p>;
-
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const getAll = () => good + neutral + bad;
-  const getAvg = () => {
-    return good || neutral || bad
-      ? (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
-      : 0;
-  };
-  const getPositive = () => {
-    return good || neutral || bad
-      ? (good / (good + neutral + bad)) * 100 + " %"
-      : 0;
-  };
-
-  const valuesArr = [good, neutral, bad];
-  const textArr = ["good", "neutral", "bad"];
-  const statTextArr = ["all", "average", "positive"];
-
-  if (!getAll()) {
-    return (
-      <>
-        <FeedbackHead headText="Give feedback" />
-        <Button text={textArr[0]} handleClick={() => setGood(good + 1)} />
-        <Button text={textArr[1]} handleClick={() => setNeutral(neutral + 1)} />
-        <Button text={textArr[2]} handleClick={() => setBad(bad + 1)} />
-        <StatText text="Statistics" />
-        <NoFeedback text="No feedback given" />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <FeedbackHead headText="Give feedback" />
-        <Button text={textArr[0]} handleClick={() => setGood(good + 1)} />
-        <Button text={textArr[1]} handleClick={() => setNeutral(neutral + 1)} />
-        <Button text={textArr[2]} handleClick={() => setBad(bad + 1)} />
-        <StatText text="Statistics" />
-        <StatisticsTable
-          valuesArr={valuesArr}
-          textsArr={textArr}
-          statTextArr={statTextArr}
-          all={getAll()}
-          avg={getAvg()}
-          positive={getPositive()}
-        />
-      </>
-    );
-  }
 };
 
 export default App;
