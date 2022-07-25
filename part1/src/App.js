09a11fd064d028afd8cd6f6834b70b05bb410da7
ProgSaleh <progsaleh@hotmail.com>
2022-07-25 03:25:58 +0300

    solve: 1.7: unicafe step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,21 +14,37 @@ const FeedbackValue = ({ text, value }) => (
   </div>
 );
 
+const FeedbackStatValue = ({ text, value }) => (
+  <div>
+    <span>{text}</span> <span>{value}</span>
+  </div>
+);
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const average =
+    good || neutral || bad
+      ? (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+      : 0;
+  const positive =
+    good || neutral || bad ? (good / (good + neutral + bad)) * 100 + " %" : 0;
+
   return (
     <>
-      <FeedbackHead headText={"Give feedback"} />
-      <Button text={"good"} handleClick={() => setGood(good + 1)} />
-      <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
-      <Button text={"bad"} handleClick={() => setBad(bad + 1)} />
-      <StatText text={"Statistics"} />
-      <FeedbackValue text={"good"} value={good} />
-      <FeedbackValue text={"neutral"} value={neutral} />
-      <FeedbackValue text={"bad"} value={bad} />
+      <FeedbackHead headText="Give feedback" />
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <StatText text="Statistics" />
+      <FeedbackValue text="good" value={good} />
+      <FeedbackValue text="neutral" value={neutral} />
+      <FeedbackValue text="bad" value={bad} />
+      <FeedbackStatValue value={good + neutral + bad} text="all" />
+      <FeedbackStatValue value={average} text="average" />
+      <FeedbackStatValue value={positive} text="positive" />
     </>
   );
 };
