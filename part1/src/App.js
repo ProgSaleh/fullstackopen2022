c119a35d81f18daa9cd69e48faeaa48dfc2cdc08
ProgSaleh <progsaleh@hotmail.com>
2022-07-25 03:06:51 +0300

    solve: 1.6: unicafe step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,35 @@
 import { useState } from "react";
 
-const Display = ({ value }) => <div>{value}</div>;
+const FeedbackHead = ({ headText }) => <h1>{headText}</h1>;
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const App = () => {
-  const [value, setValue] = useState(10);
+const StatText = ({ text }) => <p>{text}</p>;
 
-  const setToValue = (newValue) => {
-    console.log("value now", newValue);
-    setValue(newValue);
-  };
+const FeedbackValue = ({ text, value }) => (
+  <div>
+    <span>{text}</span> <span>{value}</span>
+  </div>
+);
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
-    <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text={"thousand"} />
-      <Button handleClick={() => setToValue(0)} text={"reset"} />
-      <Button handleClick={() => setToValue(value + 1)} text={"increment"} />
-    </div>
+    <>
+      <FeedbackHead headText={"Give feedback"} />
+      <Button text={"good"} handleClick={() => setGood(good + 1)} />
+      <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
+      <Button text={"bad"} handleClick={() => setBad(bad + 1)} />
+      <StatText text={"Statistics"} />
+      <FeedbackValue text={"good"} value={good} />
+      <FeedbackValue text={"neutral"} value={neutral} />
+      <FeedbackValue text={"bad"} value={bad} />
+    </>
   );
 };
 
