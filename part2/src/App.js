b3e21686fdff8ad95fff99a34ae8254984ac779b
ProgSaleh <progsaleh@hotmail.com>
2022-08-06 16:37:48 +0300

    2.8: The Phonebook Step3

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,44 +1,52 @@
 import { useState } from "react";
+import Form from "./components/Form";
 
-const Person = ({ person }) => <p>{person.name}</p>;
+const Person = ({ person }) => (
+  <p>
+    {person.name} {person.phone}
+  </p>
+);
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Saleh Hussain" }]);
+  const [persons, setPersons] = useState([
+    { name: "Saleh Hussain", phone: "0500050055" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addPerson = (event) => {
-    setNewName(event.target.value);
-  };
+  const addPerson = (event) => setNewName(event.target.value);
 
-  const addName = (event) => {
+  const addNumber = (event) => setNewNumber(event.target.value);
+
+  const addFullPerson = (event) => {
+    if (!newName || !newNumber) {
+      event.preventDefault(); // to prevent reloading the page...
+      return;
+    }
     if (persons.some((p) => (p.name === newName ? true : false))) {
-      alert(`${newName} is already addid to phonebook!`);
+      alert(`(${newName}) is already added to phonebook!`);
       event.preventDefault(); // to prevent reloading the page...
       setNewName("");
       return;
     }
     event.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, phone: newNumber };
     // setPersons(persons.concat(newPerson));
     setPersons([...persons, newPerson]);
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name:{" "}
-          <input
-            /* no need for value...*/ value={newName}
-            onChange={addPerson}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Form
+        addName={addFullPerson}
+        newName={newName}
+        addPerson={addPerson}
+        newNumber={newNumber}
+        addNumber={addNumber}
+      />
       <h2>numbers</h2>
       {persons.map((p) => (
         <Person
---
+++ part2/src/components/Form.js
@@ -0,0 +1,32 @@
+const Form = (props) => (
+  <form onSubmit={props.addName}>
+    <NameControl newName={props.newName} addPerson={props.addPerson} />
+    <br />
+    <PhoneNumberControl
+      newNumber={props.newNumber}
+      addNumber={props.addNumber}
+    />
+    <br />
+    <SubmitControl />
+  </form>
+);
+
+const NameControl = ({ newName, addPerson }) => (
+  <div>
+    name: <input value={newName} onChange={addPerson} />
+  </div>
+);
+
+const PhoneNumberControl = ({ newNumber, addNumber }) => (
+  <div>
+    number: <input value={newNumber} onChange={addNumber} />
+  </div>
+);
+
+const SubmitControl = () => (
+  <div>
+    <button type="submit">add</button>
+  </div>
+);
+
+export default Form;
