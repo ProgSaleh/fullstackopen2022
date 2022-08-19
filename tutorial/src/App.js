27ab9d8514b0c4065f6ab1cbda591fae677c37c7
ProgSaleh <progsaleh@hotmail.com>
2022-08-20 00:44:27 +0300

    tutorial; Adding styles to React app

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,11 +1,38 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/Notes";
+import "./index.css";
+
+const Notification = ({ msg }) => {
+  if (msg === null) {
+    return null;
+  }
+
+  return <div className="error">{msg}</div>;
+};
+
+const Footer = () => {
+  const footerStyle = {
+    color: "crimson",
+    fontStyle: "italic",
+    fontSize: "16",
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2022
+      </em>
+    </div>
+  );
+};
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
-  // const [showAll, setShowAll] = useState(true);
+  const [showAll, setShowAll] = useState(true);
+  const [errorMsg, setErrorMsg] = useState("Some error happend...");
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -24,7 +51,12 @@ const App = () => {
         setNotes(notes.map((n) => (n.id !== id ? n : returnNote)));
       })
       .catch((err) => {
-        alert(`the note ${note.content} was already deleted!`);
+        setErrorMsg(`Note ${note.content} was already removed from server!`);
+
+        setTimeout(() => {
+          setErrorMsg(null);
+        }, 5000);
+        // alert(`the note ${note.content} was already deleted!`);
       });
     setNotes(notes.filter((n) => n.id !== id));
   };
@@ -52,6 +84,7 @@ const App = () => {
   return (
     <>
       <h1>Notes</h1>
+      <Notification msg={errorMsg} />
       <ul>
         {notes.map((note) => (
           <Note
@@ -65,6 +98,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </>
   );
 };
