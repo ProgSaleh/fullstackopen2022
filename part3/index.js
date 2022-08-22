728598878300eafd00c0b3de9cbcf9c5971bee2e
ProgSaleh <progsaleh@hotmail.com>
2022-08-22 09:55:52 +0300

    3.9 phonebook backend step9

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan"); // I actually don't like morgan!
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 let persons = [
   {
